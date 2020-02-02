e279679f40580a0f0f2354c80dc7632620b0a165
kato yuko <yy_yuko_121@yahoo.co.jp>
2020-02-02 10:37:47 +0900

    ユーザー作成&ログイン実装

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
       patch :confirm
     end
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  resources :users, only: [:new, :create, :show]
 end
